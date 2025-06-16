08b5e26d70ed038ba4dc08125bcd49f426378ae7
Vinay970147 <vinay.datascience24@gmail.com>
2025-06-16 19:08:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 import pandas as pd
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -9,6 +9,8 @@ st.write(
   """)
 from snowflake.snowpark.functions import col, when_matched
 from snowflake.snowpark import Session
+cnx=st.connection("snowflake")
+session = cnx.session()
 og_dataset = session.table("smoothies.public.orders")
 editable_df = pd.DataFrame([
     {"ORDER_UID": "123", "ORDER_FILLED": "Yes"},
@@ -30,9 +32,6 @@ og_dataset.merge(
 name_on_order = st.text_input("Name on Smoothie!:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-cnx=st.connection("snowflake")
-session=cnx.session()
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
