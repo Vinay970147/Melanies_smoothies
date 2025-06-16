03cb34068f6064608128e04454de75508c925421
Vinay970147 <vinay.datascience24@gmail.com>
2025-06-16 22:17:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,9 @@ if ingredients_list:
   ingredients_string = ''
   for fruit_chosen in ingredients_list:
     ingredients_string +=fruit_chosen + ' '
+
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen,'search_on'].iloc[0]
+    
     st.subheader(fruit_chosen+'Nutrition Information')
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
-    sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
+    fruityvice_response = requests.get("https://my.fruityice.com/api/fruit/"+ search_on)
+    fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width= True)
